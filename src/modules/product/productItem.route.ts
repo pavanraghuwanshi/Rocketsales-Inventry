e93f14f9c6531666695fa9216f0bf79c3ce9e455
Pavan Raghuwanshi <pavanraghuwanshi77@gmail.com>
2026-04-02 18:12:17 +0530

    all stock get

--- src/modules/product/productItem.route.ts
+++ src/modules/product/productItem.route.ts
@@ -1,13 +1,14 @@
 import { Hono } from "hono";
 import { verifyToken } from "../../middleware/auth.middleware";
 
-import { addProductItems, getProductItemsByRack, uploadProductItemsExcel } from "./productItem.controller";
+import { addProductItems, getProductItems, getProductItemsByRack, uploadProductItemsExcel } from "./productItem.controller";
 
 const productItemRoutes = new Hono();
 
 productItemRoutes.use("*", verifyToken);
 
 productItemRoutes.post("/", addProductItems);
+productItemRoutes.get("/all-stocks", getProductItems);
 productItemRoutes.get("/rack-wise", getProductItemsByRack);
 productItemRoutes.post("/upload-excel", uploadProductItemsExcel);
 
